9bd4a2d71d25880bffbe1a96612dd21e3fe5e863
berkaycit <berkaycit@gmail.com>
2025-12-25 20:17:46 +0300

    Initial Commit

---
+++ zenban/zenbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  zenbanApp.swift
+//  zenban
+//
+//  Created by Berkay Çit on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct zenbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ zenbanTests/zenbanTests.swift
@@ -0,0 +1,17 @@
+//
+//  zenbanTests.swift
+//  zenbanTests
+//
+//  Created by Berkay Çit on 25.12.2025.
+//
+
+import Testing
+@testable import zenban
+
+struct zenbanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
